07674f9457e81fa91f804bee7ecabe7f53406467
Refaliya Vinandha Shalsabila <revaliya80@gmail.com>
2026-09-14 00:07:11 +0700

    Tugas

---
+++ Jobsheet2/Anggota.java
@@ -0,0 +1,43 @@
+package Jobsheet2;
+
+public class Anggota {
+    private String nomorKTP;
+    private String nama;
+    private int limitPeminjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String nomorKTP, String nama, int limitPeminjaman) {
+        this.nomorKTP = nomorKTP;
+        this.nama = nama;
+        this.limitPeminjaman = limitPeminjaman;
+        this.jumlahPinjaman = 0;
+    }
+
+    public String getNama() {
+    return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPeminjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int jumlah) {
+        if (jumlahPinjaman + jumlah > limitPeminjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPinjaman += jumlah;
+        }
+    }
+
+    public void angsur(int jumlah) {
+    if (jumlah < jumlahPinjaman * 0.1) {
+        System.out.println("Maaf, angsuran harus 10% dari jumlah pinjaman");
+    } else {
+        jumlahPinjaman -= jumlah;
+    }
+}
+}
