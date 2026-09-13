ec31dbb85cae1a2046e1ffc0073fcfdf731ac64f
Refaliya Vinandha Shalsabila <revaliya80@gmail.com>
2026-09-13 23:47:09 +0700

    Modifikasi Nomor 5

--- Jobsheet2/Motor.java
+++ Jobsheet2/Motor.java
@@ -29,7 +29,9 @@ public class Motor {
     }
 
     public void setKecepatan(int kecepatan) {
-    if (!this.statusMesin && kecepatan > 0) {
+    if (kecepatan < 0) {
+        System.out.println("Kecepatan tidak boleh bernilai negatif");
+    } else if (!this.statusMesin && kecepatan > 0) {
         System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
     } else if (kecepatan > 100) {
         System.out.println("Kecepatan maksimal adalah 100");
